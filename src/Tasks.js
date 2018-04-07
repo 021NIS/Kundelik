01737e0d9f0808ecada9296fdd0e6f96bca5aed0
Islam Temirbek <aitemr16@gmail.com>
2018-04-07 15:59:47 +0600

    feat(App): add navigation

---
+++ src/Tasks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Tasks extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Kundelik</Text>
+      </View>
+    );
+  }
+}
+
+export default Tasks;
